d02cee0b0dd5453ca5e36d17f9782c691d74d99c
Caio Gilles <209029622+CaioGilles@users.noreply.github.com>
2026-08-25 20:13:03 -0300

    Feat: criado a classe main

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("   Bem-vindo à Mesa de DJ");
+        System.out.println("=================================");
+        System.out.println("1 - Tocar instrumento");
+        System.out.println("2 - Parar instrumento");
+        System.out.println("3 - Listar instrumentos");
+        System.out.println("0 - Sair");
+        System.out.println("=================================");
+    }
+}
