dbb5a63bf4d5a954f25f9bddc7bd5aee4274cd9d
gustavo veloso <gugaveloso12@gmail.com>
2026-08-26 19:18:41 -0300

    Feat: Instrumento agora implementa Runnable com metodo run()

--- src/Instrumento.java
+++ src/Instrumento.java
@@ -1,10 +1,35 @@
-public class Instrumento {
+public class Instrumento implements Runnable {
     private String nome;
-    private boolean tocando;
+    private volatile boolean tocando;
+    private Thread thread;
 
     public Instrumento(String nome, boolean tocando) {
         this.nome = nome;
         this.tocando = tocando;
+    }
+
+    /**
+     * Inicia a thread do instrumento. Chamar mais de uma vez nao tem efeito.
+     */
+    public synchronized void iniciar() {
+        if (thread == null) {
+            thread = new Thread(this, nome);
+            thread.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("🎵 " + nome + " tocando...");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
     }
 
     public String getNome() {
